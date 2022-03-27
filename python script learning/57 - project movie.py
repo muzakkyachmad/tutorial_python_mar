3c8db924116ee927242ac06f476c220e2a51e42b
zuko <muzakkyachmad28@gmail.com>
2022-03-27 22:58:46 +0200

    update

---
+++ python script learning/57 - project movie.py
@@ -0,0 +1,42 @@
+MENU_PROMPT = "\nEnter 'a' to add movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
+movies = []
+
+def add_movie():
+    title = input("Enter the movie title: ")
+    director = input("Enter the movie director: ")
+    year = input("Enter the movie release year: ")
+
+    movies.append({
+        'title': title,
+        'director': director,
+        'year': year
+    })
+
+
+def show_movies():
+    for movie in movies:
+        print_movie(movie)
+
+
+def print_movie(movie):
+    print(f"Title: {movie['title']}")
+    print(f"Director: {movie['director']}")
+    print(f"Year: {movie['year']}")
+
+
+def find_movie():
+    search_title = input("Enter movie title you are looking for")
+
+    for movie in movies:
+        if movie["title"] == search_title
+            print_movie(movie)
+
+
+
+def menu():
+    selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == "a":
+            add_movie()
+        elif selection == "l":
+            show_movies()
