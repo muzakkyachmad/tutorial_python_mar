842409db8010a0fd5deef8f82d670b397fdab38c
zakky-ihe <amu004@un-ihe.org>
2022-03-31 16:06:54 +0200

    oop update 64- 66

--- python script learning/4_OOP/64 - magic.py
+++ python script learning/4_OOP/64 - magic.py
@@ -15,11 +15,40 @@ class Garage:
     def __init__(self):
         self.cars = []
 
+
     def __len__(self):
         return len(self.cars)
+
+
+    def __getitem__(self, i):
+        return self.cars[i]
+
+
+    def __repr__(self):
+        return f'<Garage with {len(self)}>'
 
 ford = Garage()
 ford.cars.append('fiesta')
 ford.cars.append('focus')
 
-print(ford.cars)
+
+print(ford[1]) #to call Garage.__getitem__(ford, 0)
+
+
+for car in ford:
+    print(car)
+
+
+#exercise magic in python
+
+class Club:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+        
+
+some_club = Club('Arsenal')
+my_club.players.append('Rolf')
+my_club.players.append('Anne')
+
+my
